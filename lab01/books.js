154dcacdb738186d1160a0fa72b47f009b6666e0
LamisTarek <lameeswaheed99@gmail.com>
2023-05-15 23:04:09 +0300

    d01

---
+++ lab01/books.js
@@ -0,0 +1,92 @@
+let books = [
+    {
+      ISBN: "9780747532743",
+      name: "Harry Potter and the Philosophers",
+      category: "Fantasy",
+      image: "https://images.moviesanywhere.com/143cdb987186a1c8f94d4f18de211216/fdea56fa-2703-47c1-8da8-70fc5382e1ea.jpg",
+      numPages: 80,
+      author: "J.K. Rowling",
+      price: 9.99
+    },
+    {
+      ISBN: "9780140276336",
+      name: "To Kill a Mockingbird",
+      category: "Fiction",
+      image: "https://upload.wikimedia.org/wikipedia/commons/4/4f/To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg",
+      numPages: 40,
+      author: "Harper Lee",
+      price: 12.99
+    },
+    {
+      ISBN: "9781984855453",
+      name: "Atomic Habits",
+      category: "Self-help",
+      image: "https://m.media-amazon.com/images/I/81bGKUa1e0L._AC_UF1000,1000_QL80_.jpg",
+      numPages: 320,
+      author: "James Clear",
+      price: 16.99
+    },
+    {
+      ISBN: "9780593318271",
+      name: "Project Hail Mary",
+      category: "Science Fiction",
+      image: "https://m.media-amazon.com/images/I/81gUA3NPH6L.jpg",
+      numPages: 496,
+      author: "Andy Weir",
+      price: 20.99
+    },
+    {
+      ISBN: "9780062650500",
+      name: "The Subtle Art of Not Giving a F*ck",
+      category: "Self-help",
+      image: "https://m.media-amazon.com/images/I/71QKQ9mwV7L._AC_UF1000,1000_QL80_.jpg",
+      numPages: 224,
+      author: "Mark Manson",
+      price: 14.99
+    },
+    {
+      ISBN: "9780062457714",
+      name: "Magic of Tidying Up",
+      category: "Self-help",
+      image: "https://m.media-amazon.com/images/I/91Jh-PEp1OL._AC_UF1000,1000_QL80_.jpg",
+      numPages: 224,
+      author: "Marie Kondo",
+      price: 10.99
+    },
+    {
+      ISBN: "9780199537259",
+      name: "Pride and Prejudice",
+      category: "Romance",
+      image: "https://prodimage.images-bn.com/pimages/9781499369748_p0_v3_s1200x630.jpg",
+      numPages: 480,
+      author: "Jane Austen",
+      price: 8.99
+    },
+    {
+      ISBN: "9780141182605",
+      name: "Nineteen Eighty-Four",
+      category: "Science Fiction",
+      image: "https://m.media-amazon.com/images/I/51Ul1Wg3nBL.jpg",
+      numPages: 336,
+      author: "George Orwell",
+      price: 11.99
+    },
+    {
+      ISBN: "9780062316097",
+      name: "Yes Please",
+      category: "Autobiography",
+      image: "https://m.media-amazon.com/images/I/81H+oGFPKpL.jpg",
+      numPages: 352,
+      author: "Amy Poehler",
+      price: 13.99
+    },
+    {
+      ISBN: "9781101885802",
+      name: "The Handmaid's Tale",
+      category: "Science Fiction",
+      image: "https://m.media-amazon.com/images/I/71mfL5OGNNL._AC_UF1000,1000_QL80_.jpg",
+      numPages: 336,
+      author: "Margaret Atwood",
+      price: 9.99
+    }
+  ];
